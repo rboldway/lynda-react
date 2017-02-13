d5affc4b322575ad7c172954119621d232c2eb55
Richard Boldway <richard.boldway@gmail.com>
2017-02-13 12:14:28 -0600

    Adding new notes via ui

--- Richard/js/Note.js
+++ Richard/js/Note.js
@@ -57,13 +57,13 @@ var Board = React.createClass({
     },
     getInitialState: function() {
         return {
-            notes: [
-                'Call Bill',
-                'Email Lisa',
-                'Make dentist appt',
-                'Send Proposal'
-            ]
+            notes: []
         };
+    },
+    add: function(text) {
+        var arr = this.state.notes;
+        arr.push(text);
+        this.setState({notes: arr});
     },
     update: function(newText, i) {
         var arr = this.state.notes;
@@ -87,6 +87,8 @@ var Board = React.createClass({
     render: function() {
         return (<div className="board">
                     {this.state.notes.map(this.eachNote)}
+                    <button className="btn btn-sm btn-success glyphicon glyphicon-plus"
+                            onClick={this.add.bind(null, "New Note")}/>
             </div>
 
         );
