02d39207a7c2ab640da4c6a3724a0e689d4398bd
Richard Boldway <richard.boldway@gmail.com>
2017-02-14 17:03:32 -0600

    ES6 function naming refactor

--- Richard/js/Note.js
+++ Richard/js/Note.js
@@ -1,54 +1,50 @@
 var Note = React.createClass({
-    getInitialState: function() {
+    getInitialState() {
         return {editing: false}
     },
-    componentWillMount: function() {
+    componentWillMount() {
         this.style = {
             right: this.randomBetween(0, window.innerWidth - 150) + 'px',
             top: this.randomBetween(0, window.innerHeight - 150) + 'px',
             transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
         };
     },
-    componentDidMount: function(){
+    componentDidMount() {
         $(this.getDOMNode()).draggable();
     },
-    randomBetween: function(min, max) {
+    randomBetween(min, max) {
         return (min + Math.ceil(Math.random() * max));
     },
-    edit: function() {
+    edit() {
         this.setState({editing: true});
     },
-    save: function() {
+    save() {
         this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
         this.setState({editing: false});
     },
-    remove: function() {
+    remove() {
         this.props.onRemove(this.props.index);
     },
-    renderDisplay: function() {
+    renderDisplay() {
         return (
-            <div className="note"
-                style={this.style}>
+            <div className="note" style={this.style}>
                 <p>{this.props.children}</p>
                 <span>
-                    <button onClick={this.edit}
-                            className="btn btn-primary glyphicon glyphicon-pencil"/>
-                    <button onClick={this.remove}
-                            className="btn btn-danger glyphicon glyphicon-trash"/>
+                    <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
                 </span>
             </div>
             );
     },
-    renderForm: function() {
+    renderForm() {
         return (
             <div className="note" style={this.style}>
-            <textarea ref="newText" defaultValue={this.props.children} 
-            className="form-control"></textarea>
-            <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+	            <textarea ref="newText" defaultValue={this.props.children} className="form-control"/>
+	            <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
             </div>
             )
     },
-    render: function() {
+    render() {
         if (this.state.editing) {
             return this.renderForm();
         }
@@ -69,16 +65,16 @@ var Board = React.createClass({
             }
         }
     },
-    getInitialState: function() {
+    getInitialState() {
         return {
             notes: []
         };
     },
-    nextId: function() {
+    nextId() {
         this.uniqueId = this.uniqueId || 0;
         return this.uniqueId++;
     },
-    componentWillMount: function() {
+    componentWillMount() {
         var self = this;
         if(this.props.count) {
             $.getJSON("http://baconipsum.com/api/?type=all-meat&sentences=" +
@@ -89,7 +85,7 @@ var Board = React.createClass({
                 });
         }
     },
-    add: function(text) {
+    add(text) {
         var arr = this.state.notes;
         arr.push({
             id: this.nextId(),
@@ -97,17 +93,17 @@ var Board = React.createClass({
         });
         this.setState({notes: arr});
     },
-    update: function(newText, i) {
+    update(newText, i) {
         var arr = this.state.notes;
         arr[i].note = newText;
         this.setState({notes:arr});
     },
-    remove: function(i) {
+    remove(i) {
         var arr = this.state.notes;
         arr.splice(i, 1);
         this.setState({notes: arr});
     },
-    eachNote: function(note, i) {
+    eachNote(note, i) {
         return (
                 <Note key={note.id}
                     index={i}
@@ -116,7 +112,7 @@ var Board = React.createClass({
                 >{note.note}</Note>
             );
     },
-    render: function() {
+    render() {
         return (<div className="board">
                     {this.state.notes.map(this.eachNote)}
                     <button className="btn btn-sm btn-success glyphicon glyphicon-plus"
